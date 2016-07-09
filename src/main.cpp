936cdcfdfc3062253ec568377a3e1f3f18e5b022
WinPooh32 <winpooh32@yandex.ru>
2016-07-09 12:10:08 +1000

    Code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@
 #include "Player.h"
 #include "Map.h"
 #include "GUI/Text.h"
-//#include "Tree.h"
 
 Map* map;
 
@@ -27,11 +26,94 @@ Player* player;
 
 int edit_tile = TILE_GRASS;
 
-Text* current_tile;
+Text* label_current_tile;
 
 bool edit_mode = false;
 
 Timer keys_timer;
+
+float mgrid = 1.0f / 32.0f;
+
+void SetCurrentBrushTile(int tile){
+    edit_tile = tile;
+
+    switch (edit_tile) {
+        case TILE_GRASS:
+            label_current_tile->SetText("GRASS");
+            break;
+        case TILE_PLAIN_GRASS:
+            label_current_tile->SetText("PLAIN_GRASS");
+            break;
+        case TILE_TREE:
+            label_current_tile->SetText("TREE");
+            break;
+        case TILE_TREE_SPRUCE:
+            label_current_tile->SetText("TREE SPRUCE");
+            break;
+        case TILE_WATER:
+            label_current_tile->SetText("WATER");
+            break;
+        case TILE_MOUNTAIN:
+            label_current_tile->SetText("MOUNTAIN");
+            break;
+        case TILE_BUILDING:
+            label_current_tile->SetText("BUILDING");
+            break;
+        default:
+            label_current_tile->SetText("UNKNOWN");
+    }
+}
+
+void ProcessEditModeCamera(){
+    int x, y;
+    Mouse::GetPos(&x, &y);
+
+    if (x < 10) {
+        Window::GetCamera()->Move(Vec2(-5, 0));
+    } else if (x > Window::GetWidth() - 10) {
+        Window::GetCamera()->Move(Vec2(5, 0));
+    }
+
+    if (y < 10) {
+        Window::GetCamera()->Move(Vec2(0, -5));
+    } else if (y > Window::GetHeight() - 10) {
+        Window::GetCamera()->Move(Vec2(0, 5));
+    }
+
+    Vec2 cam_offset(Window::GetCamera()->X(), Window::GetCamera()->Y());
+
+    if (Mouse::Pressed(MOUSE_LEFT)) {
+        map->SetTile((Mouse::GetPos() + cam_offset) * mgrid, static_cast<tile> (edit_tile));
+    }
+}
+
+void ProcessEditMode(SDL_Event *event){
+    //Switch brush by mouse wheel
+    if (Mouse::AnyWheeled()) {
+        if (Mouse::Wheeled(MOUSE_WHEEL_UP)) {
+            ++edit_tile;
+        } else {
+            --edit_tile;
+        }
+
+        if (edit_tile >= TILE_NONE) {
+            edit_tile = TILE_GRASS;
+        } else if (edit_tile < 0) {
+            edit_tile = TILE_NONE - 1;
+        }
+
+        SetCurrentBrushTile(edit_tile);
+    }
+    //Set brush by keyboard's number
+    else if(event->type == SDL_KEYDOWN
+            && event->key.keysym.scancode >= SDL_SCANCODE_1
+            && event->key.keysym.scancode <= SDL_SCANCODE_0){
+
+        int number = (event->key.keysym.scancode - SDL_SCANCODE_1) % 10;
+
+        SetCurrentBrushTile(number);
+    }
+}
 
 void Engine::OnInit() {
     Resources::SetDefaultFont("PressStart2P.ttf");
@@ -55,111 +137,42 @@ void Engine::OnInit() {
     player = new Player(map);
     layer_player->Connect(player);
 
-    current_tile = new Text();
-    current_tile->Init(10, 10, "GRASS", Resources::GetDefaultFont(), 14);
+    label_current_tile = new Text();
+    label_current_tile->Init(10, 10, "GRASS", Resources::GetDefaultFont(), 14);
 }
 
 void Engine::OnEvent(SDL_Event *event, const Uint8 *keyboardState) {
-
-}
-
-float mgrid = 1.0f / 32.0f;
-
-void Engine::OnUpdate() {
-
-    keys_timer.Start();
-    if (keys_timer.GetTime() > 100) {
-
-        if (Keyboard::isKeyDown(KEY_E)) {
+    if(event->type == SDL_KEYDOWN){
+        if (Keyboard::isKeyDown(KEY_E)){
             edit_mode = !edit_mode;
             player->_freecam = edit_mode;
         }
-        keys_timer.Stop();
+
+        if (Keyboard::isKeyDown(KEY_ESCAPE)){
+            Engine::Stop();
+        }
     }
 
     if (Mouse::InWindow() && edit_mode) {
-        int x, y;
-        Mouse::GetPos(&x, &y);
-
-        if (x < 10) {
-            Window::GetCamera()->Move(Vec2(-5, 0));
-        } else if (x > Window::GetWidth() - 10) {
-            Window::GetCamera()->Move(Vec2(5, 0));
-        }
-
-        if (y < 10) {
-            Window::GetCamera()->Move(Vec2(0, -5));
-        } else if (y > Window::GetHeight() - 10) {
-            Window::GetCamera()->Move(Vec2(0, 5));
-        }
-
-        Vec2 camoffset(Window::GetCamera()->X(), Window::GetCamera()->Y());
-        //tile mtile = map->GetTile((Mouse::GetPos() + camoffset) * mgrid);
-
-        if (Mouse::Pressed(MOUSE_LEFT)) {
-            map->SetTile((Mouse::GetPos() + camoffset) * mgrid, static_cast<tile> (edit_tile));
-        }
-
-        if (Mouse::AnyWheeled()) {
-            if (Mouse::Wheeled(MOUSE_WHEEL_UP)) {
-                ++edit_tile;
-            } else {
-                --edit_tile;
-            }
-
-            if (edit_tile >= TILE_NONE) {
-                edit_tile = TILE_GRASS;
-            } else if (edit_tile < 0) {
-                edit_tile = TILE_NONE - 1;
-            }
-
-            switch (edit_tile) {
-                case TILE_GRASS:
-                    std::cout << "GRASS" << std::endl;
-                    current_tile->SetText("GRASS");
-                    break;
-                case TILE_PLAIN_GRASS:
-                    std::cout << "PLAIN_GRASS" << std::endl;
-                    current_tile->SetText("PLAIN_GRASS");
-                    break;
-                case TILE_TREE:
-                    std::cout << "TREE" << std::endl;
-                    current_tile->SetText("TREE");
-                    break;
-                case TILE_TREE_SPRUCE:
-                    std::cout << "TREE SPRUCE" << std::endl;
-                    current_tile->SetText("TREE SPRUCE");
-                    break;
-                case TILE_WATER:
-                    std::cout << "WATER" << std::endl;
-                    current_tile->SetText("WATER");
-                    break;
-                case TILE_MOUNTAIN:
-                    std::cout << "MOUNTAIN" << std::endl;
-                    current_tile->SetText("MOUNTAIN");
-                    break;
-                case TILE_BUILDING:
-                    std::cout << "BUILDING" << std::endl;
-                    current_tile->SetText("BUILDING");
-                    break;
-                default:
-                    std::cout << "UNKNOWN" << std::endl;
-                    current_tile->SetText("UNKNOWN");
-            }
-
-        }
+        ProcessEditMode(event);
     }
 
+}
+
+void Engine::OnUpdate() {
+    if (edit_mode) {
+        ProcessEditModeCamera();
+    }
 }
 
 void Engine::OnRender() {
     map->Draw();
 
     if (edit_mode) {
-        Vec2 camoffset(Window::GetCamera()->X(), Window::GetCamera()->Y());
-        map->DrawDebugTileRect((Mouse::GetPos() + camoffset) * mgrid, static_cast<tile> (edit_tile));
+        Vec2 cam_offset(Window::GetCamera()->X(), Window::GetCamera()->Y());
+        map->DrawDebugTileRect((Mouse::GetPos() + cam_offset) * mgrid, static_cast<tile> (edit_tile));
 
-        current_tile->Draw();
+        label_current_tile->Draw();
     }
 }
 
